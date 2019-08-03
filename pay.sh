6548389b409bcc636673cdfb3e603191a67f5f9e
Harman Singh <harman28@gmail.com>
2019-08-03 11:22:47 +0530

    Updates pay script to skip country code

--- pay.sh
+++ pay.sh
@@ -8,7 +8,7 @@ curl -X POST https://vasooli.herokuapp.com/webhook \
     -d "payload[payment][entity][id]=pay_$id" \
     -d "payload[payment][entity][amount]=1000" \
     -d "payload[payment][entity][acquirer_data][rrn]=91$time" \
-    -d "payload[payment][entity][notes][agent_id]=919823967243"
+    -d "payload[payment][entity][notes][agent_id]=9823967243"
 
 # '{
 #   "event": "payment.authorized",
